b2d3d0f1f432c25791250342f48f48cbc70985c1
Edwin Mejia <eddwin@live.com>
2015-03-22 19:34:42 +0800

    initialized instance variables

--- credit_card.rb
+++ credit_card.rb
@@ -9,6 +9,10 @@ class CreditCard
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above (do not forget the '@')
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
