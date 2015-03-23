dd29cbaccde9ccb648f09850e92de00d82c1dcb2
Edwin Mejia <eddwin@live.com>
2015-03-23 13:27:53 +0800

    Algorithm in place

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,14 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    checksum = nums_a.pop
+    nums_a.reverse!
+    nums_a = nums_a.map.with_index do |n,i|
+      i.even? ? n*2 : n*1
+    end
+    single = nums_a.to_s.chars.map(&:to_i)
+    sum = single.inject(:+)
+    luhn = sum + checksum
+    luhn%10==0? true : (puts "Usage:  Credit Card number invalid")
   end
 end
