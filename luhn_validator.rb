6e7bb38a964b9dec1ed96ebf94045127b46ff9ba
Edwin Mejia <eddwin@live.com>
2015-03-25 10:49:32 +0800

    minor tweaks

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -10,9 +10,8 @@ module LuhnValidator
     nums_a = nums_a.map.with_index do |n,i|
       i.even? ? n*2 : n*1
     end
-    single = nums_a.to_s.chars.map(&:to_i)
-    sum = single.inject(:+)
-    luhn = sum + checksum
+    nums_a = nums_a.to_s.chars.map(&:to_i)
+    luhn = nums_a.inject(:+) + checksum
     luhn%10==0? (puts "Usage:  Credit Card number valid, Hooray!") : (puts "Usage:  Credit Card number invalid")
   end
 end
