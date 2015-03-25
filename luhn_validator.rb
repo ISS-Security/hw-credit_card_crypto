0982c11243a3c2de2ba04f2a5185a1e451c0f083
rojotic26 <rojotic26@gmail.com>
2015-03-25 21:51:40 +0800

    Updating credit_card and luhn_validator

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,8 +4,16 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
+    #number = 4539075978941247
     nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    checksum = nums_a.pop
+    nums_a.reverse!
+    nums_a = nums_a.map.with_index do |n,i|
+      i.even? ? n*2 : n*1
+    end
+    single = nums_a.to_s.chars.map(&:to_i)
+    sum = single.inject(:+)
+    luhn = sum + checksum
+    luhn%10==0? true : (puts "Usage:  Credit Card number invalid")
   end
 end
