e3e8c504b06c767bdadca7caea75d2de8f94d863
Edwin Mejia <eddwin@live.com>
2015-04-02 15:06:32 +0800

    added luhn_validator to this repo

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -14,4 +14,27 @@ module LuhnValidator
     luhn = single.inject(:+) + checksum
     luhn % 10 == 0 ? true : false
   end
+  def validate_checksum2
+    nums_a = number.to_s.chars.map(&:to_i)
+total_sum = nums_a.pop
+double_digit = []
+a_rev = nums_a.reverse!
+a_rev.map!.with_index(0){ |x, index| if index % 2 == 0
+2*x
+else
+1*x end}
+a_rev.each { |x|
+if x > 9
+double_digit = x.to_s.chars.each_slice(1).map{|a| a.join.to_i}
+total_sum = total_sum + double_digit[0] + double_digit[1]
+else
+total_sum = total_sum + x
+end
+}
+if total_sum % 10 == 0
+return true
+else
+return false
+end
+  end
 end
