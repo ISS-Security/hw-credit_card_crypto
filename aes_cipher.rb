0ceacc1724212aa9e7fc9816c29d483ec74f1e4f
Edwin Mejia <eddwin@live.com>
2015-04-09 01:56:25 +0800

    added AES cipher

--- aes_cipher.rb
+++ aes_cipher.rb
@@ -1,15 +1,22 @@
 require 'openssl'
-
+require 'json'
 module AesCipher
   def self.encrypt(document, key)
-    # TODO: Return JSON string of array: [iv, ciphertext]
-    #       where iv is the random intialization vector used in AES
-    #       and ciphertext is the output of AES encryption
-    # NOTE: Use hexadecimal strings for output so that it is screen printable
-    #       Use cipher block chaining mode only!
+
+    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    cipher.encrypt
+    cipher.key = key
+    iv = cipher.random_iv
+    ciphertext = cipher.update(document) + cipher.final
+    aes_crypt = [iv.unpack("H*"),ciphertext.unpack("H*")].to_json
   end
 
-  def self.encrypt(aes_crypt, key)
-    # TODO: decrypt from JSON output (aes_crypt) of encrypt method above
+  def self.decrypt(aes_crypt, key)
+    decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    recieve = JSON.parse(aes_crypt)
+    decipher.decrypt
+    decipher.key = key
+    decipher.iv = recieve[0].pack("H*")
+    plain = decipher.update(recieve[1].pack("H*")) + decipher.final
   end
 end
