da16149f2a91b5459956bd9a7bd04b56197634b7
jeydlex <alexebarahona@yahoo.es>
2015-04-09 17:15:48 +0800

    Fixed names - shuffle (Random) is not predictible

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -20,23 +20,23 @@ module DoubleTranspositionCipher
 	 new_sorted_columns = sorted_columns.shuffle(random: Random.new(key))
 	 
 	 # 4.3. final matrix
-	 final_matrix = (final_col.transpose()).to_a	 
+	 final_matrix = (new_sorted_columns.transpose()).to_a	 
 	 return final_matrix.join
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
     # 1. find number of rows/cols such that matrix is almost square
-     rows = Math.sqrt(document.length).round #for making almost a perfect square
-     cols = (document.length/rows.to_f).round #for finding the number of columns of the matrix
+     rows = Math.sqrt(ciphertext.length).round #for making almost a perfect square
+     cols = (ciphertext.length/rows.to_f).round #for finding the number of columns of the matrix
 
      # 2. break plaintext into evenly sized blocks
-     doc_blocks = document.split('').map(&:to_s).each_slice(cols).to_a
+     doc_blocks = ciphertext.split('').map(&:to_s).each_slice(cols).to_a
      
      # 3. get the original columns of the array
      doc_blocks_mat = (doc_blocks.transpose()).to_a
      
-     #4. sort columns of each row in predictibly random way
+     #4. sort columns of each row in predictibly random way -SHUFFLE IS NOT PREDICTABLE, IT IS RANDOM
 	 new_blocks = doc_blocks_mat.shuffle(random: Random.new(key))
 	
 	 # 4.1. transpose columns and make them array	
@@ -44,6 +44,6 @@ module DoubleTranspositionCipher
 	
 	 # 4.2 this assigns a random order to the columns based on the key	
 	 original_doc = original_end.shuffle(random: Random.new(key))
-	 return oridinal_doc.join
+	 return original_doc.join
   end
 end
