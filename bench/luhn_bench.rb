2acbe44d23b69db38fcfa69df01a60e3a34dc94b
Soumya Ray <soumya.ray@gmail.com>
2016-03-06 18:36:05 +0800

    Add first bench test for Luhn algorithm

---
+++ bench/luhn_bench.rb
@@ -0,0 +1,20 @@
+require 'benchmark'
+require_relative '../credit_card'
+
+def self.benchtest
+  Benchmark.bm(15) do |bench|
+    n = 200_000
+    card_number_length = 16
+
+    card_numbers = n.times.map do
+      card_number_length.times.map { Random.rand(10).to_s }.join
+    end
+
+    bench.report('luhn_benchmark') do
+      card_numbers.each do |number|
+        cc = CreditCard.new(number, nil, nil, nil)
+        cc.validate_checksum
+      end
+    end
+  end
+end
