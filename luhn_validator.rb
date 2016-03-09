ecad54d3473ea8d408cac0780297c4a68fa1da6f
rjollet- <renaudjollet@gmail.com>
2016-03-09 18:00:47 +0800

    checksum validator
    First methode to validate the number
    
    Signed-off-by: rjollet- <renaudjollet@gmail.com>

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,7 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
+    nums_a.reverse.map.with_index { |n, i| i%2==0 ? n : (n>4 ? (n*2)-9 : (n*2))}.reduce(&:+) % 10 == 0
     # TODO: use the integers in nums_a to validate its last check digit
   end
 end
