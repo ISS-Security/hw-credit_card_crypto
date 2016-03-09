fd1662d85e4592e54c56aae509cd8b4fe52cf39e
rjollet- <renaudjollet@gmail.com>
2016-03-09 20:35:41 +0800

    Improve the algorithm inspired by Nicholas works
    Signed-off-by: rjollet- <renaudjollet@gmail.com>

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,8 +4,12 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-    nums_a.reverse.map.with_index { |n, i| i%2==0 ? n : (n>4 ? (n*2)-9 : (n*2))}.reduce(&:+) % 10 == 0
-    # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = number.to_s.reverse.chars.map(&:to_i)
+    sum = 0
+    nums_a.each_slice(2) do |odd, even|
+      sum += odd if odd
+      sum += even>4 ? (even*2)-9 : (even*2) if even
+    end
+    return sum % 10 == 0
   end
 end
