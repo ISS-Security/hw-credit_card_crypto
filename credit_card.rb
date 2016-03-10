81afcd09cde3fe9534c373737ef6587f74218525
Jherez Taylor <JherezTaylor@users.noreply.github.com>
2016-03-10 17:58:36 +0800

    Update credit_card.rb

--- credit_card.rb
+++ credit_card.rb
@@ -20,6 +20,10 @@ class CreditCard
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
      }.to_json
   end
 
