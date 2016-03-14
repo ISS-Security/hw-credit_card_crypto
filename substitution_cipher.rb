06deeea17a77780a62c93aa6cccf016f87f41cd6
KuanYuChen <david819162002@yahoo.com.tw>
2016-03-15 00:02:29 +0800

    finish permutation

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -27,6 +27,8 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+	  permutation_table = Array(0..127).shuffle(random: Random.new(key)) # create the permutation table
+	  document.each_byte.map{ |char| permutation_table[char].chr}.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +38,8 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+	  permutation_table = Array(0..127).shuffle(random: Random.new(key)) 
+	  document.each_byte.map{|char| permutation_table.index(char).chr}.join # decrypt the real value according to the index
     end
   end
 end
