45f051e6f14652026ac8c22879ff0b9442378e86
Soumya Ray <soumya.ray@gmail.com>
2016-03-19 23:06:49 +0800

    New tests for hashing

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,34 +1,37 @@
 require_relative '../credit_card'
 require 'minitest/autorun'
 
-describe 'Test hashing requirements' do
-  before do
-    @cc1 = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
-    @cc2 = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
-    @cc3 = CreditCard.new('5423661657234057', 'Feb-30-2020', 'Soumya Ray', 'Mastercard')
-  end
+# Feel free to replace the contents of cards with data from your own yaml file
+card_details = [
+  { num: '4916603231464963', exp: 'Mar-30-2020', name: 'Soumya Ray', net: 'Visa' },
+  { num: '6011580789725897', exp: 'Sep-30-2020', name: 'Nick Danks', net: 'Visa' },
+  { num: '5423661657234057', exp: 'Feb-30-2020', name: 'Lee Chen', net: 'Mastercard' }
+]
 
+cards = card_details.map { |c| CreditCard.new(c[:num], c[:exp], c[:name], c[:net]) }
+
+describe 'Test hashing requirements' do
   describe 'Test regular hashing' do
-    it 'should find the same hash for identical cards' do
-      # TODO: implement this test
+    describe 'Check hashes are consistently produced' do
+      # TODO: Check that each card produces the same hash if hashed repeatedly
     end
 
-    it 'should produce different hashes for different information' do
-      # TODO: implement this test
+    describe 'Check for unique hashes' do
+      # TODO: Check that each card produces a different hash than other cards
     end
   end
 
   describe 'Test cryptographic hashing' do
-    it 'should find the same hash for identical cards' do
-      # TODO: implement this test
+    describe 'Check hashes are consistently produced' do
+      # TODO: Check that each card produces the same hash if hashed repeatedly
     end
 
-    it 'should produce different hashes for different information' do
-      # TODO: implement this test
+    describe 'Check for unique hashes' do
+      # TODO: Check that each card produces a different hash than other cards
     end
 
-    it 'should not produce the same regular vs. cryptographic hash' do
-      # TODO: implement this test
+    describe 'Check regular hash not same as cryptographic hash' do
+      # TODO: Check that each card's hash is different from its hash_secure
     end
   end
 end
