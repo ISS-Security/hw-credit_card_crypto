a398cb5ec22fa04f4a30cdf5013620ac262288fa
IsmaelNoble <ismaelnoble@gmail.com>
2017-03-07 22:39:38 +0800

    Lun validator and some other stuff

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,9 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    check_sum = nums_a.pop
+    nums_a.map! { |e| e.odd? ? e * 2 : e * 1 }
+    check_sum == (nums_a.sum * 9) % 10
   end
+  # TODO: use the integers in nums_a to validate its last check digitend
 end
