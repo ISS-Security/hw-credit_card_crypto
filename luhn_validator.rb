f00f23eb87c7f49eb4ac7ec7f163adee2957c830
IsmaelNoble <ismaelnoble@gmail.com>
2017-03-08 09:00:46 +0800

    updated validator, still wierd though

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -6,8 +6,17 @@ module LuhnValidator
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
     check_sum = nums_a.pop
-    nums_a.map! { |e| e.odd? ? e * 2 : e * 1 }
+    if nums_a.length.even?
+      nums_a.map!.with_index { |e, i| i.odd? ? e * 2 : e * 1 }
+    else
+      nums_a.map!.with_index { |e, i| i.even? ? e * 2 : e * 1 }
+    end
+    nums_a.map! { |a| a > 9 ? sum_digits(a) : a * 1 }
     check_sum == (nums_a.sum * 9) % 10
   end
   # TODO: use the integers in nums_a to validate its last check digitend
+
+  def sum_digits(var)
+  newvar = var.to_s.split('').map(&:to_i).inject { |e, f| e + f }
+  end
 end
