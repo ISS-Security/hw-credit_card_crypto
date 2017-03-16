1aefba0632d063faf1e0db3b605228de2cd59d85
frank1234211 <frank1234211@gmail.com>
2017-03-16 14:07:57 +0800

    finish partA

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,24 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    sum = 0
+
+    nums_a = nums_a.each_index do |i|
+      index_from_right = nums_a.size() - i
+      increment = 0
+
+      if index_from_right%2==0
+        increment = nums_a[i] * 2
+        if increment >9
+          increment -= 9
+        end
+      else
+        increment = nums_a[i]
+      end
+      sum +=  increment
+    end
+
+    judge = sum%10==0? true:false
   end
+
 end
