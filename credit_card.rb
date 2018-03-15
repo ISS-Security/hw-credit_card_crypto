4131a9c15d243cedc053f403d94e92abc6d16e8d
Abner Tellez <atellezsazo@gmail.com>
2018-03-15 14:08:13 +0800

    PART A
    PART A

--- credit_card.rb
+++ credit_card.rb
@@ -3,18 +3,24 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
-    # TODO: initialize the instance variables listed above
+    @number=number
+    @expiration_date=expiration_date
+    @owner=owner
+    @credit_network=credit_network
   end
 
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
+      "number" => @number,
+      "expiration_date" => @expiration_date,
+      "owner" => @owner,
+      "credit_network" => @credit_network,
     }.to_json
   end
 
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,12 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
+    check_number = nums_a.pop
 
-    # TODO: use the integers in nums_a to validate its last check digit
+    check_sum = nums_a.reverse.each_slice(2).flat_map do |a, b|
+      [(a * 2).divmod(10), b || 0]
+    end.flatten.inject(:+)
+
+    check_number.zero? ? check_sum % 10 == 0 : (10 - check_sum % 10) == check_number
   end
 end
