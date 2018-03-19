609c158412be014e485cdba83ce54ffe243876e6
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2018-03-19 21:55:41 +0800

    Update

--- credit_card.rb
+++ credit_card.rb
@@ -3,18 +3,23 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
-
+  
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      :number => @number, :expiration_date => @expiration_date,:owner => @owner, :credit_network => @credit_network
     }.to_json
   end
 
@@ -26,6 +31,7 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    JSON.parse(card_s)
   end
 
   # return a hash of the serialized credit card object
