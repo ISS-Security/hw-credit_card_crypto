fb895b300f4c23e56db6a65fd33110cef4c9f901
Orison <orison09@gmail.com>
2018-03-21 06:26:25 +0900

    added some stuff3

--- test_area.rb
+++ test_area.rb
@@ -5,23 +5,19 @@ def self.encrypt(document, key)
     # 1. find number of rows/cols such that matrix is almost square
     text = document.split('')
     size =  text.size 
-    key_int = key.to_s.bytes.reduce(&:*)
+    #key_int = key.to_s.bytes.reduce(&:*) Do we need this line? 
     matrix_size = Math.sqrt(size).ceil
-
-
-    # 2. break plaintext into evenly sized blocks
-    chunks = text.each_slice(3).to_a
     
+    # 2. break plaintext into evenly sized blocks
+    chunks = text.each_slice(matrix_size).to_a
     print chunks
     
     # 3. sort rows in predictibly random way using key as seed
-    puts 
-    seed = Random.new(key_int)
-    puts seed 
-    
+    rows = (0..chunks.size).to_a.shuffle(random: Random.new(key.to_i))
+    puts rows 
     # 4. sort columns of each row in predictibly random way
     
     # 5. return joined cyphertext
 end
 
-self.encrypt("mynameisorison","orison")
+self.encrypt("mynameisorison","ori")
