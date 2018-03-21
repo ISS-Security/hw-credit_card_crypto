ca0404b8a63dab43e48d2e2c1d8b7ce10b8395ad
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2018-03-21 22:37:28 +0800

    Updated

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -5,8 +5,11 @@ module SubstitutionCipher
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
+    @alphabet = ('a'..'z').to_a.join
     def self.encrypt(document, key)
-      # TODO: encrypt string using caesar cipher
+      i = key % @alphabet.size 
+      encrypt = @alphabet[i..-1] + @alphabet[0...i]
+      document.tr(@alphabet, encrypt)
     end
 
     # Decrypts String document using integer key
@@ -15,7 +18,42 @@ module SubstitutionCipher
     #   key: Fixnum (integer)
     # Returns: String
     def self.decrypt(document, key)
-      # TODO: decrypt string using caesar cipher
+      i = key % @alphabet.size 
+      encrypt = @alphabet[i..-1] + @alphabet[0...i]
+      document.tr(encrypt, @alphabet)
+    end
+  end
+
+  module Permutation
+    # Encrypts document using key
+    # Arguments:
+    #   document: String
+    #   key: Fixnum (integer)
+    # Returns: String
+    def self.encrypt(document, key)
+      # TODO: encrypt string using a permutation ciphermodule SubstitutionCipher
+  module Caesar
+    # Encrypts document using key
+    # Arguments:
+    #   document: String
+    #   key: Fixnum (integer)
+    # Returns: String
+    @@alphabet = ('a'..'z').to_a.join
+    def self.encrypt(document, key)
+      i = key % @alphabet.size 
+      encrypt = @alphabet[i..-1] + @alphabet[0...i]
+      document.tr(@alphabet, encrypt)
+    end
+
+    # Decrypts String document using integer key
+    # Arguments:
+    #   document: String
+    #   key: Fixnum (integer)
+    # Returns: String
+    def self.decrypt(document, key)
+      i = key % @alphabet.size 
+      encrypt = @alphabet[i..-1] + @alphabet[0...i]
+      document.tr(encrypt, @alphabet)
     end
   end
 
@@ -27,6 +65,19 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+    end
+
+    # Decrypts String document using integer key
+    # Arguments:
+    #   document: String
+    #   key: Fixnum (integer)
+    # Returns: String
+    def self.decrypt(document, key)
+      # TODO: decrypt string using a permutation cipher
+    end
+  end
+end
+
     end
 
     # Decrypts String document using integer key
