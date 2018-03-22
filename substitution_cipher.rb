1cf31b6454019128206e646ab07e8d60b7e34f91
An-Ting Hsu <andyhsu10@gmail.com>
2018-03-23 00:09:31 +0800

    fix caeser cipher overflow problem

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,7 +7,7 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
-      document.each_char.map { |c| (c.ord + key).chr }.join
+      document.each_char.map{ |char| ((char.ord - 32 + key % 95) % 95 + 32).chr }.join
     end
 
     # Decrypts String document using integer key
@@ -17,7 +17,7 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
-      document.each_char.map { |c| (c.ord - key).chr }.join
+      document.each_char.map{ |char| ((char.ord - 32 - key % 95) % 95 + 32).chr }.join
     end
   end
 
@@ -29,12 +29,7 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
-      hash = (0..127).to_a
-                     .shuffle(random: Random.new(key))
-                     .each_with_index
-                     .map { |n, idx| [idx, n] }
-                     .to_h
-      document.each_char.map { |c| hash[c.ord].chr }.join
+      
     end
 
     # Decrypts String document using integer key
@@ -44,12 +39,7 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
-      hash = (0..127).to_a
-                     .shuffle(random: Random.new(key))
-                     .each_with_index
-                     .map { |n, idx| [n, idx] }
-                     .to_h
-      document.each_char.map { |c| hash[c.ord].chr }.join
+      
     end
   end
 end
