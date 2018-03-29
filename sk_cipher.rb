ddda05e5ca77bc7417ee2d8bb69330ea105eda9f
Orison <orison09@gmail.com>
2018-03-29 20:38:20 +0800

    Updating SK Cipher

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -4,7 +4,7 @@ require "base64"
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
-    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    @new_key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
   end
 
   def self.encrypt(document, key)
@@ -12,16 +12,17 @@ module ModernSymmetricCipher
     #       Use base64 for ciphertext so that it is sendable as text
       simple_box = RbNaCl::SimpleBox.from_secret_key(self.generate_new_key())
       my_secret = simple_box.encrypt(document)
-      Base64.strict_encode64(my_secret)
+      Base64.encode64(my_secret)
   end
 
   def self.decrypt(encrypted_cc, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
-    decrpyto = Base64.strict_decode64(encrypted_cc)
-    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
-    simple_box.decrypt(decrytpo)
+    decrypto = Base64.decode64(encrypted_cc)
+    simples_box = RbNaCl::SimpleBox.from_secret_key(@new_key)
+    simples_box.decrypt(decrypto)
   end
+
 end
 
 cifra =  self.encrypt("This is my message","ori")
