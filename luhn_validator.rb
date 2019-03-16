c8d6e7fae68302dadced142a75fa01d1719b5f90
Lisa Dong <lisa.dong@gapp.nthu.edu.tw>
2019-03-16 23:22:49 +0800

    Complete Part A

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -6,6 +6,24 @@ module LuhnValidator
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
 
-    # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = multiply_2(nums_a)
+    nums_a = digit_sum(nums_a)
+    sum = nums_a[1..-1].sum
+
+    nums_a[0] == 10 - (sum % 10)
+  end
+
+  private
+
+  # Multiply every second digit (counting from the rightmost digit)
+  # in the array by 2
+  def multiply_2(array)
+    # [1,2,3,4] -> [4,3,2,1] -> [4,6,2,2] -> [2,2,6,4]
+    array.reverse.map.with_index { |n, i| i.even? ? n : n * 2 }.reverse
+  end
+
+  # Sum up the digits if the number is over one digit.
+  def digit_sum(array)
+    array.map { |n| n >= 10 ? n / 10 + n % 10 : n }
   end
 end
