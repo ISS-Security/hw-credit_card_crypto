fa3f67a05d177deed4c24b124428b8650d52479d
u10000129 <u10000129@gmail.com>
2019-03-20 19:25:54 +0800

    factorize double_trans_cipher.rb

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,64 +1,90 @@
+# frozen_string_literal: true
+
+# Double Transposition Cipher
 module DoubleTranspositionCipher
-  def self.encrypt(document, key)
-    # 1. find number of rows/cols such that matrix is almost square
-    str_len = document.to_s.length
+  def self.decide_row_col(text)
+    str_len = text.length
     row = Integer.sqrt(str_len)
-    col = row + 1
+    col = (Math.sqrt(str_len) % 1).zero? ? row : row + 1
+    [row, col]
+  end
 
-    # 2. break plaintext into evenly sized blocks
-    arr = document.to_s.split('')
-    arr = arr.each_slice(col).to_a
-    (col - arr[row - 1].length).times { arr[row - 1] << ' ' }
+  def self.to_double_array(text, col)
+    arr = text.split('')
+    arr.each_slice(col).to_a
+  end
 
-    # 3. sort rows in predictibly random way using key as seed
-    prng = Random.new(key)
-    key.times do
-      s_r1 = prng.rand(row)
-      s_r2 = prng.rand(row)
+  def self.generate_switch_index(random_seed, row, col)
+    prng = Random.new(random_seed)
+    switch_index = []
+    (random_seed * 2).times { switch_index << prng.rand(row) }
+    (random_seed * 2).times { switch_index << prng.rand(col) }
+    switch_index
+  end
+
+  def self.swap_row(start_idx, end_idx, switch_index, arr)
+    (start_idx...end_idx).step(2).each do |idx|
+      s_r1 = switch_index[idx]
+      s_r2 = switch_index[idx + 1]
       arr[s_r1], arr[s_r2] = arr[s_r2], arr[s_r1]
     end
+    arr
+  end
 
-    # 4. sort columns of each row in predictibly random way
+  def self.swap_col(start_idx, end_idx, switch_index, arr)
     arr = arr.transpose
-    key.times do
-      c_r1 = prng.rand(col)
-      c_r2 = prng.rand(col)
+    (start_idx...end_idx).step(2).each do |idx|
+      c_r1 = switch_index[idx]
+      c_r2 = switch_index[idx + 1]
       arr[c_r1], arr[c_r2] = arr[c_r2], arr[c_r1]
     end
-    arr = arr.transpose
+    arr.transpose
+  end
+
+  def self.fill_last_row_with_block(arr, row, col)
+    (col - arr[row - 1].length).times { arr[row - 1] << ' ' }
+    arr
+  end
+
+  def self.encrypt(document, key)
+    # 1. find number of rows/cols such that matrix is almost square
+    row, col = decide_row_col(document.to_s)
+
+    # 2. break plaintext into evenly sized blocks
+    arr = to_double_array(document.to_s, col)
+    arr = fill_last_row_with_block(arr, row, col)
+
+    # 2.5 generate switch index
+    switch_index = generate_switch_index(key, row, col)
+
+    # 3. sort rows in predictibly random way using key as seed
+    arr = swap_row(0, key * 2, switch_index, arr)
+
+    # 4. sort columns of each row in predictibly random way
+    arr = swap_col(key * 2, key * 4, switch_index, arr)
 
     # 5. return joined cyphertext
     arr.flatten!.join('')
   end
 
   def self.decrypt(ciphertext, key)
-    str_len = ciphertext.length
-    row = Integer.sqrt(str_len)
-    col = row + 1
+    # 1. find number of rows/cols
+    row, col = decide_row_col(ciphertext)
 
-    prng = Random.new(key)
-    switch_index = []
-    (key * 2).times { switch_index << prng.rand(row) }
-    (key * 2).times { switch_index << prng.rand(col) }
+    # 2. generate switch index
+    switch_index = generate_switch_index(key, row, col)
     switch_index = switch_index.reverse
 
-    arr = ciphertext.split('')
-    arr = arr.each_slice(col).to_a
+    # 3. break plaintext into evenly sized blocks
+    arr = to_double_array(ciphertext, col)
 
-    arr = arr.transpose
-    (0...key * 2).step(2).each do |idx|
-      c_r1 = switch_index[idx]
-      c_r2 = switch_index[idx + 1]
-      arr[c_r1], arr[c_r2] = arr[c_r2], arr[c_r1]
-    end
-    arr = arr.transpose
+    # 4. swap columns back
+    arr = swap_col(0, key * 2, switch_index, arr)
 
-    (key * 2...key * 4).step(2).each do |idx|
-      s_r1 = switch_index[idx]
-      s_r2 = switch_index[idx + 1]
-      arr[s_r1], arr[s_r2] = arr[s_r2], arr[s_r1]
-    end
+    # 5. swap rows back
+    arr = swap_row(key * 2, key * 4, switch_index, arr)
 
+    # 6. return decipher text
     arr.flatten!.join('').strip
   end
 end
