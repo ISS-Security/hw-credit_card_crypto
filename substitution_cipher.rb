ff49164a324f8e075efb0255b6de2bb72775e29f
rainyjonne <rainyjonne@gmail.com>
2021-03-21 10:14:49 +0800

    Add permutation encrypt & decrypt function

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -38,6 +38,7 @@ module SubstitutionCipher
     end
   end
 
+  # Permutation encrypt and decrypt
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -46,6 +47,25 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      # Split the string to array
+      chars = document.to_s.split('')
+
+      # get a table of elements ascii 0~127 in ascending order
+      ord_tab = (0..127).to_a.map(&:chr)
+
+      # get a shuffle table (use key as seed to stay the same table)
+      shuf_tab = ord_tab.shuffle(random: Random.new(key))
+
+      # get a lookup table
+      look_tab = Hash[ord_tab.zip(shuf_tab)]
+
+      # encrypt the chars
+      encrypt_arr = chars.map do |char|
+        look_tab[char]
+      end
+
+      # return an encrypted string
+      encrypt_arr.join('')
     end
 
     # Decrypts String document using integer key
@@ -55,6 +75,25 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      # Split the string to array
+      encrpt_chars = document.to_s.split('')
+
+      # get a table of elements ascii 0~127 in ascending order
+      ord_tab = (0..127).to_a.map(&:chr)
+
+      # get a shuffle table (use key as seed to stay the same table)
+      shuf_tab = ord_tab.shuffle(random: Random.new(key))
+
+      # get a reverse lookup table
+      rev_look_tab = Hash[shuf_tab.zip(ord_tab)]
+
+      # get back original string arr
+      ori_arr = encrpt_chars.map do |enc_char|
+        rev_look_tab[enc_char]
+      end
+
+      # return original string
+      ori_arr.join('')
     end
   end
 end
