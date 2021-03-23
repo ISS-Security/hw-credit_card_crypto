0d3957c6f950c51217d1071fba8d19fcb4e3abb8
ShihChu <juju0411tw@gmail.com>
2021-03-23 21:10:40 +0800

    Upload double_trans file

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,4 +1,4 @@
-require "matrix"
+require 'matrix'
 
 module DoubleTranspositionCipher
 
@@ -10,24 +10,23 @@ module DoubleTranspositionCipher
     col_num_matrix = Math.sqrt(document.to_s.length).ceil
     # 2. break plaintext into evenly sized blocks
     matrix = document.to_s.chars
-                .each_slice(col_num_matrix)
-                .to_a
-                .tap{ |i| i.last.fill(" ", i.last.length, col_num_matrix - i.last.length) }
+                .each_slice(col_num_matrix).to_a
+                .tap { |i| i.last.fill(' ', i.last.length, col_num_matrix - i.last.length) }
 
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     Kernel.srand(key)
-    row_swap_key = (0..matrix.length-1).to_a.sort_by{rand}
+    row_swap_key = (0..matrix.length - 1).to_a.sort_by { rand }
 
-    Kernel.srand(key+1)
-    col_swap_key = (0..matrix[0].length-1).to_a.sort_by{rand}
+    Kernel.srand(key + 1)
+    col_swap_key = (0..matrix[0].length - 1).to_a.sort_by { rand }
 
       
-    matrix = row_swap_key.each.with_index {|r,i| row_swap_key[i] = matrix[r]}
-    matrix = col_swap_key.each.with_index {|r,i| col_swap_key[i] = matrix.transpose[r]}.transpose
+    matrix = row_swap_key.each.with_index { |r, i| row_swap_key[i] = matrix[r] }
+    matrix = col_swap_key.each.with_index { |r, i| col_swap_key[i] = matrix.transpose[r] }.transpose
 
     # 5. return joined cyphertext
-    encrpted_document = matrix.join
+    matrix.join
   end
 
   def self.decrypt(ciphertext, key)
@@ -35,20 +34,19 @@ module DoubleTranspositionCipher
     col_num_matrix = Math.sqrt(ciphertext.to_s.length).ceil
 
     de_matrix = ciphertext.to_s.chars
-                          .each_slice(col_num_matrix)
-                          .to_a
-    
+                          .each_slice(col_num_matrix).to_a
+
     Kernel.srand(key)
-    row_swap_key = (0..de_matrix.length-1).to_a.sort_by{rand}
-    Kernel.srand(key+1)
-    col_swap_key = (0..de_matrix[0].length-1).to_a.sort_by{rand}
-                          
+    row_swap_key = (0..de_matrix.length - 1).to_a.sort_by { rand }
+    Kernel.srand(key + 1)
+    col_swap_key = (0..de_matrix[0].length - 1).to_a.sort_by { rand }
+
     mt = []
     m = []
-                    
-    col_swap_key.each.with_index {|r,i|  mt[r] = de_matrix.transpose[i]}
-    row_swap_key.each.with_index {|r,i|  m[r] =  mt.transpose[i]}
-                          
-    decrpted_document = m.join.strip
+    col_swap_key.each.with_index { |r, i|  mt[r] = de_matrix.transpose[i] }
+    row_swap_key.each.with_index { |r, i|  m[r] = mt.transpose[i] }
+
+    # return
+    m.join.strip
   end
 end
