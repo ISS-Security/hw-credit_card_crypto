14bbc0e934010d2d66a9065b75ceeeeb58f9afd1
tara-thomas <t.tarathomas@gmail.com>
2021-03-23 14:18:15 +0800

    Implemented Part A: Luhn Algo

--- credit_card.rb
+++ credit_card.rb
@@ -1,21 +1,32 @@
-require_relative './luhn_validator.rb'
+require_relative './luhn_validator'
 require 'json'
 
+# credit card class
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*options)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
-    }.to_json
+      'creditcard' => {
+        'number' => @number,
+        'expiration_date' => @expiration_date,
+        'owner' => @owner,
+        'credit_network' => @credit_network
+      }
+    }.to_json(*options)
   end
 
   # returns all card information as single string
@@ -26,6 +37,8 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    info = JSON.parse(card_s)['creditcard']
+    CreditCard.new(info['number'], info['expiration_date'], info['owner'], info['credit_network'])
   end
 
   # return a hash of the serialized credit card object
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,9 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
+    nums_a.reverse!
+    number = nums_a.map.with_index { |num, index| index.even? ? num : (num * 2).digits.sum }
+    (number.reduce(:+) % 10).zero?
     # TODO: use the integers in nums_a to validate its last check digit
   end
 end
