e5f3730b8ee5fd0a85438d9d23277b68a22729fe
RayhungKao <ryamkao2339@gmail.com>
2022-03-05 20:51:21 +0800

    pass all tests for luhn_validator

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'bundler-audit'
 gem 'json'
 gem 'minitest'
 # Add all the gems your code and tests use here!
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,11 +1,23 @@
+# frozen_string_literal: true
+
+# Validates credit card number using Luhn Algorithm
 module LuhnValidator
-  # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
     # TODO: use the integers in nums_a to validate its last check digit
+    iterator = 0
+    total = 0
+    nums_a.reverse.each do |digit|
+      if iterator.positive?
+        digit *= 2 if iterator.odd?
+        digit -= 9 if digit > 9
+        total += digit
+      end
+      iterator += 1
+    end
+    ((10 - (total % 10)) % 10) == nums_a[nums_a.length - 1]
   end
 end
