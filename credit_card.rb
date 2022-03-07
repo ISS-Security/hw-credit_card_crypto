2d142f69c422f488c71265c4c74f8ccf0f30d0ee
suvincent <suvincent0226@gmail.com>
2022-03-07 16:42:14 +0800

    PART A Done

--- credit_card.rb
+++ credit_card.rb
@@ -1,20 +1,31 @@
-require_relative './luhn_validator.rb'
+# frozen_string_literal: true
+
+require_relative './luhn_validator'
 require 'json'
 
+# CreditCard class
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*_args)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      number: number,
+      expiration_date: expiration_date,
+      owner: owner,
+      credit_network: credit_network
     }.to_json
   end
 
@@ -26,6 +37,8 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    object = JSON.parse(card_s)
+    CreditCard.new(object['number'], object['expiration_date'], object['owner'], object['credit_network'])
   end
 
   # return a hash of the serialized credit card object
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Module for validating Credit Cards
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,7 +8,15 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
     # TODO: use the integers in nums_a to validate its last check digit
+    s = nums_a.reverse.each_with_index.reduce(0) do |sum, (x, i)|
+      if i.even?
+        sum + x
+      else
+        mod = x == 9 ? 9 : (x * 2 % 9)
+        sum + mod
+      end
+    end
+    (s % 10).zero?
   end
 end
