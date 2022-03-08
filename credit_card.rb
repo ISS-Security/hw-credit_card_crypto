71f5fac0ec1f3e9666804d4a606b23b4cc272d42
RayhungKao <ryamkao2339@gmail.com>
2022-03-08 14:20:37 +0800

    credit card info serialization and deserialization

--- credit_card.rb
+++ credit_card.rb
@@ -1,12 +1,15 @@
-require_relative './luhn_validator.rb'
+# frozen_string_literal: true
+
+require_relative './luhn_validator'
 require 'json'
 
+# Crypto on credit card, including error checking (data integrity) algorithm and very elementary crypto algorithms
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
   include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
-  
+
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
     @number = number
@@ -16,9 +19,13 @@ class CreditCard
   end
 
   # returns json string
-  def to_json
+  def to_json(*_args)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      number: number,
+      expiration_date: expiration_date,
+      owner: owner,
+      credit_network: credit_network
     }.to_json
   end
 
@@ -30,6 +37,9 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    card_object = JSON.parse(card_s)
+    CreditCard.new(card_object['number'], card_object['expiration_date'], card_object['owner'],
+                   card_object['credit_network'])
   end
 
   # return a hash of the serialized credit card object
@@ -38,6 +48,12 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    {
+      number: number,
+      expiration_date: expiration_date,
+      owner: owner,
+      credit_network: credit_network
+    }.to_json.hash
   end
 
   # return a cryptographically secure hash
