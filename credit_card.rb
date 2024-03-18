186d42d09a502ca9ae004ff0dcacd15ad08be0de
DarkOracle13 <bobby.setiawan2002@gmail.com>
2024-03-18 20:56:33 +0800

    feat: completing luhn algorithm

--- credit_card.rb
+++ credit_card.rb
@@ -1,21 +1,32 @@
-require_relative './luhn_validator.rb'
+# frozen_string_literal: true
+
+require_relative './luhn_validator'
 require 'json'
 
+# CreditCard class
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*args)
     {
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
       # TODO: setup the hash with all instance vairables to serialize into json
-    }.to_json
+    }.to_json(*args)
   end
 
   # returns all card information as single string
@@ -34,6 +45,7 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    @number.hash ^ @expiration_date.hash ^ @owner.hash ^ @credit_network.hash
   end
 
   # return a cryptographically secure hash
@@ -41,5 +53,11 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    Digest::SHA256.hexdigest(to_s)
   end
 end
+
+# Example usage:
+# c = CreditCard.new(1234567890123456, "04/15", "John Doe", :visa)
+# puts c.number
+# puts c.to_json
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# method for validating credit card number using Luhn Algorithm
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,7 +8,14 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
+    checksum = nums_a.pop # remove last digit
+    total = 0
+    nums_a.reverse.each_with_index do |digit, i| # iterate from right to left
+      digit *= 2 if i.even? # Double if even
+      total += digit.divmod(10).sum
+    end
+    difference = (10 - (total % 10)) % 10
+    difference == checksum
     # TODO: use the integers in nums_a to validate its last check digit
   end
 end
