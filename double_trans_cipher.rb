72e4f30aaf0c1e1493983600ec0bb5d82220e23a
Tony Yang <t510599@gmail.com>
2024-03-19 23:23:02 +0800

    feat: DoubleTranspositionCipher

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,4 +1,9 @@
 module DoubleTranspositionCipher
+  # Encrypts document using key
+  # Arguments:
+  #   document: String
+  #   key: Fixnum (integer)
+  # Returns: String
   def self.encrypt(document, key)
     # TODO: FILL THIS IN!
     ## Suggested steps for double transposition cipher
@@ -7,9 +12,42 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+    size = Math.sqrt(document.length).ceil
+    matrix = to_matrix(document, size)
+
+    key2 = Random.new(key).rand(key)
+    
+    matrix.shuffle!(random: Random.new(key))
+    matrix.each do |row|
+      row.shuffle!(random: Random.new(key2))
+    end
+    matrix.map(&:join).join
   end
 
+  # Decrypts String document using integer key
+  # Arguments:
+  #   ciphertext: String
+  #   key: Fixnum (integer)
+  # Returns: String
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
+    size = Math.sqrt(ciphertext.length).ceil
+    matrix = ciphertext.chars.each_slice(size).to_a
+
+    key2 = Random.new(key).rand(key)
+
+    key_map = (0...size).to_a.shuffle!(random: Random.new(key)) 
+    key2_map = (0...size).to_a.shuffle!(random: Random.new(key2)) 
+
+    matrix.sort_by!.with_index { |_, i| key_map[i] }
+    matrix.each do |row|
+      row.sort_by!.with_index { |_, i| key2_map[i] }
+    end
+    matrix.map(&:join).join.strip
+  end
+
+  def self.to_matrix(document, size, padding = 0.chr)
+    document << padding * (-document.length % (size * size))
+    document.chars.each_slice(size).to_a
   end
 end
