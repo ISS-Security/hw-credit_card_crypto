57977085edc511d21fd8e8e670f68bf489385618
root <root@SharonsLaptop.localdomain>
2025-03-09 11:37:50 +0800

    rubocop: 1 file inspected, no offenses detected

--- credit_card.rb
+++ credit_card.rb
@@ -3,20 +3,29 @@
 require_relative './luhn_validator'
 require 'json'
 
+# top-level documentation comment
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*_args)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
     }.to_json
   end
 
@@ -28,6 +37,9 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    card_data = JSON.parse(card_s)
+    new(card_data['number'], card_data['expiration_date'],
+        card_data['owner'], card_data['credit_network'])
   end
 
   # return a hash of the serialized credit card object
@@ -36,6 +48,12 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    {
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
+    }.hash
   end
 
   # return a cryptographically secure hash
@@ -43,5 +61,8 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    sha256 = Digest::SHA256.new
+    sha256.update(to_s)
+    sha256.hexdigest
   end
 end
