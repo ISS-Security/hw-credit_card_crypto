5fd265063189ac0d3055cd9e2ca83eb559e8709e
root <root@SharonsLaptop.localdomain>
2025-03-11 01:34:09 +0800

    rubocop: 1 file inspected, no offenses detected

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,15 +1,68 @@
+# frozen_string_literal: true
+
+# Double Transposition Cipher implementation
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
-    # TODO: FILL THIS IN!
-    ## Suggested steps for double transposition cipher
-    # 1. find number of rows/cols such that matrix is almost square
-    # 2. break plaintext into evenly sized blocks
-    # 3. sort rows in predictibly random way using key as seed
-    # 4. sort columns of each row in predictibly random way
-    # 5. return joined cyphertext
+    setup_matrix(document)
+    row_order = generate_keymap(key, @size)  # Fixed: added key param and used @size
+    apply_row_shuffle(row_order)
+    col_order = generate_keymap(key, @size)  # Fixed: added key param and used @size
+    apply_column_shuffle(col_order)
+    matrix_to_string
   end
 
+  # Fixed: changed _key to key for consistency
   def self.decrypt(ciphertext, key)
-    # TODO: FILL THIS IN!
+    setup_matrix(ciphertext)
+    row_order = generate_keymap(key, @size)  # Fixed: added key param and used @size
+    col_order = generate_keymap(key, @size)  # Fixed: added key param and used @size
+    undo_column_shuffle(col_order) # NOTE: order reversed from encrypt
+    undo_row_shuffle(row_order)
+    matrix_to_string.strip
+  end
+
+  def self.setup_matrix(text)
+    @size = Math.sqrt(text.length).ceil
+    @matrix = to_matrix(text)
+  end
+
+  def self.to_matrix(text, padding = "\0")
+    padded_length = (@size * @size)
+    text = text.dup
+    text << padding * (padded_length - text.length) if text.length < padded_length
+    text.chars.each_slice(@size).to_a
+  end
+
+  # Fixed: added key parameter
+  def self.generate_keymap(key, size)
+    (0...size).to_a.shuffle(random: Random.new(key))
+  end
+
+  def self.apply_row_shuffle(order)
+    @matrix = order.map { |i| @matrix[i] }
+  end
+
+  def self.apply_column_shuffle(order)
+    @matrix = @matrix.map do |row|
+      order.map { |i| row[i] }
+    end
+  end
+
+  def self.undo_column_shuffle(order)
+    inverse = Array.new(order.length)
+    order.each_with_index { |pos, i| inverse[pos] = i }
+    @matrix = @matrix.map do |row|
+      inverse.map { |i| row[i] }
+    end
+  end
+
+  def self.undo_row_shuffle(order)
+    inverse = Array.new(order.length)
+    order.each_with_index { |pos, i| inverse[pos] = i }
+    @matrix = inverse.map { |i| @matrix[i] }
+  end
+
+  def self.matrix_to_string
+    @matrix.map(&:join).join
   end
 end
