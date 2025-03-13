6ded0e46769b6a4b877816344b82760769fa6a52
irina <syrok.BU@yandex.ru>
2025-03-13 15:51:47 +0800

    implemented double transposition cipher

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,15 +1,42 @@
+# frozen_string_literal: true
+
+# encrypt/decrypt secrets using double transposition technique
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
-    # TODO: FILL THIS IN!
-    ## Suggested steps for double transposition cipher
-    # 1. find number of rows/cols such that matrix is almost square
-    # 2. break plaintext into evenly sized blocks
-    # 3. sort rows in predictibly random way using key as seed
-    # 4. sort columns of each row in predictibly random way
-    # 5. return joined cyphertext
+    doc_len = document.length
+    ciphertext = [''] * key
+
+    (0...key).each do |col|
+      current_idx = col
+      until current_idx >= doc_len
+        ciphertext[col] += document[current_idx]
+        current_idx += key
+      end
+    end
+
+    ciphertext.join('')
   end
 
   def self.decrypt(ciphertext, key)
-    # TODO: FILL THIS IN!
+    cols = (ciphertext.length / key.to_f).ceil.to_i
+    rows = key
+
+    original = [''] * cols
+    col = 0
+    row = 0
+
+    # matrix cells with no letters
+    empty_cells = (cols * rows) - ciphertext.length
+
+    ciphertext.split('').each do |sym|
+      original[col] += sym
+      col += 1
+      if (col == cols) || ((col == cols - 1) && (row >= rows - empty_cells))
+        col = 0
+        row += 1
+      end
+    end
+
+    original.join('')
   end
 end
