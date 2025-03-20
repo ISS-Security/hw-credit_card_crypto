2911b87146ae089ee2b576d86c2a00bbadd47c30
yuhsiangl <aalex910606@gmail.com>
2025-03-20 21:43:56 +0800

    use secretbox instead of simplebox

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -4,6 +4,8 @@ require 'rbnacl'
 
 # top-level documentation
 module ModernSymmetricCipher
+  NONCE_BYTES = RbNaCl::SecretBox.nonce_bytes
+
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
     key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
@@ -15,18 +17,22 @@ module ModernSymmetricCipher
     #       Use base64 for ciphertext so that it is sendable as text
     key = Base64.strict_decode64(key)
 
-    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
-    ciphertext = simple_box.encrypt(document)
-    Base64.strict_encode64(ciphertext)
+    secret_box = RbNaCl::SecretBox.new(key)
+    nonce = RbNaCl::Random.random_bytes(NONCE_BYTES)
+    ciphertext = secret_box.encrypt(nonce, document)
+    Base64.strict_encode64(nonce + ciphertext)
   end
 
   def self.decrypt(encrypted_cc, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
     key = Base64.strict_decode64(key)
-    ciphertext = Base64.strict_decode64(encrypted_cc)
+    secret_box = RbNaCl::SecretBox.new(key)
 
-    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
-    simple_box.decrypt(ciphertext)
+    decoded_data = Base64.strict_decode64(encrypted_cc)
+    nonce = decoded_data[0...NONCE_BYTES]
+    ciphertext = decoded_data[NONCE_BYTES..]
+
+    secret_box.decrypt(nonce, ciphertext)
   end
 end
