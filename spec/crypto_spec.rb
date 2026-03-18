b6e6ae5d9fe4ebcccc38dcf3ea435fe4fc28d3f1
Jasmine Davina <jasminedavina@gmail.com>
2026-03-19 00:39:34 +0800

    doubletrans

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
+require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 require 'minitest/rg'
 
@@ -12,34 +13,62 @@ describe 'Test card info encryption' do
     @key = 3
   end
 
-  describe 'Using Caesar cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      _(enc).wont_equal @cc.to_s
-      _(enc).wont_be_nil
-    end
+  [SubstitutionCipher::Caesar, SubstitutionCipher::Permutation, DoubleTranspositionCipher].each do |type|
+    describe "Using #{type} cipher" do
+      it 'should encrypt card information' do
+        enc = type.encrypt(@cc, @key)
+        _(enc).wont_equal @cc.to_s
+        _(enc).wont_be_nil
+      end
 
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-      _(dec).must_equal @cc.to_s
-    end
-  end
-
-  describe 'Using Permutation cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      _(enc).wont_equal @cc.to_s
-      _(enc).wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
-      _(dec).must_equal @cc.to_s
+      it 'should decrypt text' do
+        enc = type.encrypt(@cc, @key)
+        dec = type.decrypt(enc, @key)
+        _(dec).must_equal @cc.to_s
+      end
     end
   end
+  # describe 'Using Caesar cipher' do
+  #   it 'should encrypt card information' do
+  #     enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
+  #     _(enc).wont_equal @cc.to_s
+  #     _(enc).wont_be_nil
+  #   end
 
-  # TODO: Add tests for double transposition and modern symmetric key ciphers
-  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
+  #   it 'should decrypt text' do
+  #     enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
+  #     dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
+  #     _(dec).must_equal @cc.to_s
+  #   end
+  # end
+
+  # describe 'Using Permutation cipher' do
+  #   it 'should encrypt card information' do
+  #     enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+  #     _(enc).wont_equal @cc.to_s
+  #     _(enc).wont_be_nil
+  #   end
+
+  #   it 'should decrypt text' do
+  #     enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+  #     dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
+  #     _(dec).must_equal @cc.to_s
+  #   end
+  # end
+
+  # # TODO: Add tests for double transposition and modern symmetric key ciphers
+  # #       Can you DRY out the tests using metaprogramming? (see lecture slide)
+  # describe 'Using Double Transposition cipher' do
+  #   it 'should encrypt card information' do
+  #     enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+  #     _(enc).wont_equal @cc.to_s
+  #     _(enc).wont_be_nil
+  #   end
+
+  #   it 'should decrypt text' do
+  #     enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+  #     dec = DoubleTranspositionCipher.decrypt(enc, @key)
+  #     _(dec).must_equal @cc.to_s
+  #   end
+  # end
 end
