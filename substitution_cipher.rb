d2f07ca40b653190e028b7ec365b36651cef1408
charleneshawn <19.chsw@gmail.com>
2026-03-18 21:24:11 +0800

    finish permutation cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SubstitutionCipher
   module Caesar
     # Encrypts document using key
@@ -19,6 +21,7 @@ module SubstitutionCipher
     end
   end
 
+  # implement permutation cipher
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,6 +30,13 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      rng = Random.new(key)
+      shuffle = (0..127).to_a.shuffle(random: rng)
+      
+      text = document.to_s
+      text.chars.map do |ch|
+        shuffle[ch.ord].chr
+      end.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +46,17 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      rng = Random.new(key)
+      shuffle = (0..127).to_a.shuffle(random: rng)
+      reverse = {}
+      shuffle.each_with_index do |mapped, original|
+        reverse[mapped] = original
+      end
+
+      text = document.to_s
+      text.chars.map do |ch|
+        reverse[ch.ord].chr
+      end.join
     end
   end
 end
